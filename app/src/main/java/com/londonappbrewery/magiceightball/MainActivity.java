1ad36aef0d25b9541a52953ab25413699f79f188
Amir <amirgaziel0@gmail.com>
2017-09-30 14:18:30 +0300

    Project complecte!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,13 +1,56 @@
 package com.londonappbrewery.magiceightball;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Ask_Button;
+
+        Ask_Button= (Button) findViewById(R.id.Ask_Button);
+
+       final ImageView ballDispaly = (ImageView) findViewById(R.id.image_eightBall);
+
+         final int [] ballArry ={
+
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+
+
+        Ask_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomAsk = new Random();
+
+                int number = randomAsk.nextInt(5);
+
+                ballDispaly.setImageResource(ballArry[number]);
+
+            }
+        });
+
+
+
+
+
+
     }
 }
